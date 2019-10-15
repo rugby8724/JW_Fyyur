1a81cc4727892ddff37fa829910c55c3294c6c8f
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2019-10-14 20:11:17 -0500

    reformat venue/artist edit

--- app.py
+++ app.py
@@ -130,8 +130,8 @@ def search_venues():
     data = Venue.query.filter(
            Venue.name.ilike("%{}%".format(search)))
     response = {
-        "count": data.count(),
-        "data": data
+        'count': data.count(),
+        'data': data
     }
 
     return render_template('pages/search_venues.html', results=response,
@@ -145,35 +145,36 @@ def show_venue(venue_id):
 
   shows = Show.query.filter(Show.venue_id==venue_id).join(Artist, Show.artist_id == Artist.id).all()
   upcoming_shows = [{
-    "artist_id": show.artist.id,
-    "artist_name": show.artist.name,
-    "artist_image_link": show.artist.image_link,
-    "start_time": show.start_time.strftime("%Y-%m-%d %H:%M:%S")
+    'artist_id': show.artist.id,
+    'artist_name': show.artist.name,
+    'artist_image_link': show.artist.image_link,
+    'start_time': show.start_time.strftime('%Y-%m-%d %H:%M:%S')
   } for show in shows if show.start_time > datetime.now()]
 
   past_shows = [{
-    "artist_id": show.artist.id,
-    "artist_name": show.artist.name,
-    "artist_image_link": show.artist.image_link,
-    "start_time": show.start_time.strftime("%Y-%m-%d %H:%M:%S")
+    'artist_id': show.artist.id,
+    'artist_name': show.artist.name,
+    'artist_image_link': show.artist.image_link,
+    'start_time': show.start_time.strftime('%Y-%m-%d %H:%M:%S')
   } for show in shows if show.start_time <= datetime.now()]
 
   response={
-    "id": venue.id,
-    "name": venue.name,
-    "genres": venue.genres,
-    "city": venue.city,
-    "state": venue.state,
-    "phone": venue.phone,
-    "website": venue.website,
-    "facebook_link": venue.facebook_link,
-    "seeking_talent": venue.seeking_talent,
-    "seeking_description": venue.seeking_description,
-    "image_link": venue.image_link,
-    "past_shows": past_shows,
-    "upcoming_shows": upcoming_shows,
-    "past_shows_count": len(past_shows),
-    "upcoming_shows_count": len(upcoming_shows),
+    'id': venue.id,
+    'name': venue.name,
+    'city': venue.city,
+    'address': venue.address,
+    'state': venue.state,
+    'phone': venue.phone,
+    'genres': venue.genres,
+    'website': venue.website,
+    'facebook_link': venue.facebook_link,
+    'seeking_talent': venue.seeking_talent,
+    'seeking_description': venue.seeking_description,
+    'image_link': venue.image_link,
+    'past_shows': past_shows,
+    'upcoming_shows': upcoming_shows,
+    'past_shows_count': len(past_shows),
+    'upcoming_shows_count': len(upcoming_shows),
   }
   return render_template('pages/show_venue.html', venue=response)
 
@@ -254,8 +255,8 @@ def search_artists():
     data = Artist.query.filter(
            Artist.name.ilike('%' + request.form['search_term'] + '%'))
     response = {
-      "count": data.count(),
-      "data": data
+      'count': data.count(),
+      'data': data
     }
     return render_template('pages/search_artists.html', results=response,
                            search_term=request.form.get('search_term', ''))
@@ -268,35 +269,35 @@ def show_artist(artist_id):
   shows = Show.query.filter(Show.artist_id==artist_id).join(Venue, Show.venue_id == Venue.id).all()
 
   upcoming_shows = [{
-    "venue_id": show.venue.id,
-    "venue_name": show.venue.name,
-    "venue_image_link": show.venue.image_link,
-    "start_time": show.start_time.strftime("%Y-%m-%d %H:%M:%S")
+    'venue_id': show.venue.id,
+    'venue_name': show.venue.name,
+    'venue_image_link': show.venue.image_link,
+    'start_time': show.start_time.strftime('%Y-%m-%d %H:%M:%S')
   } for show in shows if show.start_time > datetime.now()]
 
   past_shows = [{
-    "venue_id": show.venue.id,
-    "venue_name": show.venue.name,
-    "venue_image_link": show.venue.image_link,
-    "start_time": show.start_time.strftime("%Y-%m-%d %H:%M:%S")
+    'venue_id': show.venue.id,
+    'venue_name': show.venue.name,
+    'venue_image_link': show.venue.image_link,
+    'start_time': show.start_time.strftime('%Y-%m-%d %H:%M:%S')
   } for show in shows if show.start_time <= datetime.now()]
 
   response={
-    "id": artist.id,
-    "name": artist.name,
-    "genres": artist.genres,
-    "city": artist.city,
-    "state": artist.state,
-    "phone": artist.phone,
-    "website": artist.website,
-    "facebook_link": artist.facebook_link,
-    "seeking_venue": artist.seeking_venue,
-    "seeking_description": artist.seeking_description,
-    "image_link": artist.image_link,
-    "past_shows": past_shows,
-    "upcoming_shows": upcoming_shows,
-    "past_shows_count": len(past_shows),
-    "upcoming_shows_count": len(upcoming_shows),
+    'id': artist.id,
+    'name': artist.name,
+    'genres': artist.genres,
+    'city': artist.city,
+    'state': artist.state,
+    'phone': artist.phone,
+    'website': artist.website,
+    'facebook_link': artist.facebook_link,
+    'seeking_venue': artist.seeking_venue,
+    'seeking_description': artist.seeking_description,
+    'image_link': artist.image_link,
+    'past_shows': past_shows,
+    'upcoming_shows': upcoming_shows,
+    'past_shows_count': len(past_shows),
+    'upcoming_shows_count': len(upcoming_shows),
   }
 
   return render_template('pages/show_artist.html', artist=response)
@@ -305,20 +306,19 @@ def show_artist(artist_id):
 #  ----------------------------------------------------------------
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
-    form = ArtistForm()
-
     artist = Artist.query.get_or_404(artist_id)
-
-    form.name.data = artist.name
-    form.city.data = artist.city
-    form.state.data = artist.state
-    form.phone.data = artist.phone
-    form.genres.data = artist.genres
-    form.image_link.data = artist.image_link
-    form.facebook_link.data = artist.facebook_link
-    form.website.data = artist.website
-    form.seeking_venue.data = artist.seeking_venue
-    form.seeking_description.data = artist.seeking_description
+    form = ArtistForm(
+    name = artist.name,
+    city = artist.city,
+    state = artist.state,
+    phone = artist.phone,
+    genres = artist.genres,
+    image_link = artist.image_link,
+    facebook_link = artist.facebook_link,
+    website = artist.website,
+    seeking_venue = artist.seeking_venue,
+    seeking_description = artist.seeking_description
+    )
   # TODO: populate form with fields from artist with ID <artist_id>
     return render_template('forms/edit_artist.html', form=form, artist=artist)
 
@@ -350,21 +350,20 @@ def edit_artist_submission(artist_id):
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
-  form = VenueForm()
-
-  venue = Venue.query.get_or_404(venue_id)
-
-  form.name.data = venue.name
-  form.city.data = venue.city
-  form.state.data = venue.state
-  form.address.data = venue.address
-  form.phone.data = venue.phone
-  form.genres.data = venue.genres
-  form.image_link.data = venue.image_link
-  form.facebook_link.data = venue.facebook_link
-  form.website.data = venue.website
-  form.seeking_talent.data = venue.seeking_talent
-  form.seeking_description.data = venue.seeking_description
+  venue = Venue.query.get(venue_id)
+  form = VenueForm(
+    name = venue.name,
+    city = venue.city,
+    state = venue.state,
+    address = venue.address,
+    phone = venue.phone,
+    genres = venue.genres,
+    image_link = venue.image_link,
+    facebook_link = venue.facebook_link,
+    website = venue.website,
+    seeking_talent = venue.seeking_talent,
+    seeking_description = venue.seeking_description
+  )
   # TODO: populate form with values from venue with ID <venue_id>
   return render_template('forms/edit_venue.html', form=form, venue=venue)
 
